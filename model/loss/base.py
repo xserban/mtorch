24fe4edbfd29778700e7fb79b4fc03bc6cb972ee
Alex <a.serban@cs.ru.nl>
2019-07-27 14:15:20 +0200

    Abstract methods

--- model/loss/base.py
+++ model/loss/base.py
@@ -1,3 +1,7 @@
 class BaseLoss(object):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
+    @abstractmethod
+    def forward(self):
+        raise NotImplementedError
--- model/metrics/base.py
+++ model/metrics/base.py
@@ -2,5 +2,10 @@ class BaseMetric(object):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
+    @abstractmethod
+    def forward(self):
+        raise NotImplementedError
+
+    @abstractmethod
     def get_name(self):
         raise NotImplementedError
