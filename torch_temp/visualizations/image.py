d9c07e8950992bde5f32a690d8abe9384905d7bb
Alex <a.serban@cs.ru.nl>
2019-09-19 12:23:13 +0200

    Simple code to display image

--- torch_temp/visualizations/image.py
+++ torch_temp/visualizations/image.py
@@ -1,1 +1,10 @@
-"""Display image"""
+import matplotlib.pyplot as plt
+
+
+def show_image(tensor_image, initiate_plt=True):
+    if initiate_plt is True:
+        plt.figure()
+        plt.imshow(tensor_image.permute(1, 2, 0))
+        plt.show()
+    else:
+        plt.imshow(tensor_image.permute(1, 2, 0))
