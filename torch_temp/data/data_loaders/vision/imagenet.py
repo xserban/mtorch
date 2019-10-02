9ad5ab9310f50b8d51dad80112fb431cf658e8d7
Alex <a.serban@cs.ru.nl>
2019-10-02 17:48:01 +0200

    Imagenet

--- torch_temp/data/data_loaders/vision/imagenet.py
+++ torch_temp/data/data_loaders/vision/imagenet.py
@@ -6,11 +6,12 @@ class ImageNetLoader(BaseDataLoader):
     """ ImageNet data loading + transformations """
 
     def __init__(self, data_dir, batch_size, shuffle=True,
-                 validation_split=0.0, num_workers=1, training=True):
+                 validation_split=0.0, num_workers=1, training=True,
+                 pin_memory=True):
         print("[INFO][DATA] \t Preparing the ImageNet dataset ...")
         if training is True:
             trans = transforms.Compose([
-                transforms.RandomCrop(224, padding=4),
+                # transforms.RandomCrop(224, padding=4),
                 transforms.RandomHorizontalFlip(),
                 transforms.ToTensor(),
                 transforms.Normalize((0.485, 0.456, 0.406),
