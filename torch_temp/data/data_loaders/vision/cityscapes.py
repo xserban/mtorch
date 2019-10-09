deb0cb00d77f5b20586dd92aa6bb147c748073e1
NullConvergence <a.serban@cs.ru.nl>
2019-10-09 16:39:48 +0200

    Implemented cityscapes db

--- torch_temp/data/data_loaders/vision/cityscapes.py
+++ torch_temp/data/data_loaders/vision/cityscapes.py
@@ -1,3 +1,7 @@
+###
+# !!! Pytorch requires to manuual download the CityScapes dataset
+# and add it to the data_dir
+###
 from torchvision import datasets, transforms
 from torch_temp.data.data_loaders.base import BaseDataLoader
 
@@ -9,16 +13,15 @@ class CityscapesLoader(BaseDataLoader):
                  validation_split=0.0,
                  num_workers=1,
                  split="train",
+                 transformations="DefaultTransformations",
                  kwargs={}):
-        print("[INFO][DATA] \t Preparing Cifar10 dataset ...")
-        if split == "train":
-            trans = transforms.Compose([
-                transforms.ToTensor(),
-            ])
-        else:
-            trans = transforms.Compose([
-                transforms.ToTensor(),
-            ])
+        print("[INFO][DATA] \t Preparing the CityScapes dataset ...")
+
+        _transf = BaseDataLoader.get_transformations(
+            self, name=transformations)
+
+        trans = _transf.get_train_trans() if split == "train" \
+            else _transf.get_test_trans()
 
         self.data_dir = data_dir
         self.dataset = datasets.Cityscapes(
