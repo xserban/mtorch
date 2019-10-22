45e9cc5045cc2299c687c108df2569cff75585b6
Alex <a.serban@cs.ru.nl>
2019-10-22 11:14:59 +0200

    Updates on viz

--- torch_temp/visualizations/image.py
+++ torch_temp/visualizations/image.py
@@ -12,12 +12,26 @@ def show_image(tensor_image, initiate_plt=True):
         plt.imshow(tensor_image.permute(1, 2, 0))
 
 
-def imshow(images):
+def convert_to_imshow_format(image):
+    image = image.numpy()
+    # convert from CHW to HWC
+    # e.g. from 3x32x32 to 32x32x3
+    return image.transpose(1, 2, 0)
+
+
+def imshow(images, file=None):
+    """Displays images in a grid. This
+      method expects the images to be unnormalized
+    :param images: array or tensor of images
+    """
+    images = [convert_to_imshow_format(x) for x in images]
     images = torchvision.utils.make_grid(images)
-    images = images / 2 + 0.5  # unnormalize
     npimg = images.numpy()
-    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.imshow(npimg)
     plt.show()
+
+    if file is not None:
+        plt.savefig(file)
 
 
 def show_image_landmarks(image, landmarks):
