ad164d1d7565225433423143b05bf9e444bbf695
Alex <a.serban@cs.ru.nl>
2019-10-22 12:26:23 +0200

    added examples folder

--- mtorch/examples/visualize.py
+++ mtorch/examples/visualize.py
@@ -1,52 +1,40 @@
-from ..core.data.data_loaders.vision import *
-from ..core.data.transformations import *
-import core.utils.util as utl
-import core.visualizations as viz
-
 import matplotlib.pyplot as plt
 
-training_dl_cifar = CIFAR10Loader(
-    "downloaded-data/",
-    batch_size=None,
-    training=True,
-    transformations="CIFARTransformations"
-)
+from core import visualizations as viz
+from core.utils import util
+from core.data.transformations import *
+from core.data.data_loaders.vision import *
 
-training_dl_default = CIFAR10Loader(
-    "downloaded-data/",
-    batch_size=None,
-    training=True,
-    transformations="DefaultTransformations"
-)
 
-testing_dl_cifar = CIFAR10Loader(
-    "downloaded-data/",
-    batch_size=None,
-    training=False,
-    transformations="CIFARTransformations"
-)
+def main():
+    training_dl_cifar = CIFAR10Loader(
+        "downloaded-data/",
+        batch_size=None,
+        training=True,
+        transformations="CIFARTransformations"
+    )
 
-testing_dl_default = CIFAR10Loader(
-    "downloaded-data/",
-    batch_size=None,
-    training=False,
-    transformations="DefaultTransformations"
-)
+    training_dl_default = CIFAR10Loader(
+        "downloaded-data/",
+        batch_size=None,
+        training=True,
+        transformations="DefaultTransformations"
+    )
 
-images, labels, _ = utl.sample_n_datapoints(training_dl_default.dataset.data,
-                                            training_dl_default.dataset.targets,
-                                            1)
+    testing_dl_cifar = CIFAR10Loader(
+        "downloaded-data/",
+        batch_size=None,
+        training=False,
+        transformations="CIFARTransformations"
+    )
 
-classes = ('plane', 'car', 'bird', 'cat', 'deer',
-           'dog', 'frog', 'horse', 'ship', 'truck')
+    testing_dl_default = CIFAR10Loader(
+        "downloaded-data/",
+        batch_size=None,
+        training=False,
+        transformations="DefaultTransformations"
+    )
 
-fig, axes = plt.subplots(1, len(images), figsize=(12, 2.5))
-for idx, image in enumerate(images):
-    axes[idx].imshow(utl.convert_to_imshow_format(image))
-    axes[idx].set_title(classes[labels[idx]])
-    axes[idx].set_xticks([])
-    axes[idx].set_yticks([])
-
-print('SHOWING IMAGES')
-
-print('STOP')
+    images, labels, _ = util.sample_n_datapoints(training_dl_default.dataset.data,
+                                                 training_dl_default.dataset.targets,
+                                                 1)
