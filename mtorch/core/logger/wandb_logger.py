b9f3740748344f6dffc5fffa45c301f1f828591c
Alex <a.serban@cs.ru.nl>
2019-11-01 23:10:07 +0100

    Updated wandb

--- mtorch/core/logger/wandb_logger.py
+++ mtorch/core/logger/wandb_logger.py
@@ -1,14 +1,15 @@
 """Weights and Biases www.wandb.com Logger"""
-from .base import BaseLogger
 import wandb
+import os
+
+from .base import BaseLogger
 
 
 class WANDBLogger(BaseLogger):
-    def __init__(self, config, pylogger):
+    def __init__(self, config):
         print("[INFO][LOGS] \t Initializing Weights and Biases Logger ...")
         super().__init__()
         self.config = config
-        self.pylogger = pylogger
         self._configure(self.config)
 
     def _configure(self, config):
@@ -17,13 +18,15 @@ class WANDBLogger(BaseLogger):
         try:
             wandb.init(name=self.config["run_name"],
                        project=self.config["project_name"],
-                       config=self.config.config)
+                       config=self.config,
+                       dir=self.config["training"]["save_dir"])
             self.log_index_batches = self.l_config["index_batches"]
             self.log_params = self.l_config["log_params"]
             self.log_train_images = self.l_config["log_train_images"]
             self.log_test_images = self.l_config["log_test_images"]
         except Exception as e:
-            self.pylogger.error("Failed to initialize W&B Logger {}".format(e))
+            print("[ERROR] \t Failed to initialize W&B Logger {}".format(e))
+            raise e
 
     def _log_info(self, step, env, loss, custom_metrics, images=None):
         wandb.log({env + "." + "loss": loss}, step=step)
@@ -37,10 +40,9 @@ class WANDBLogger(BaseLogger):
             self._log_info(step, env, loss, custom_metrics, images)
 
     def log_custom_metrics(self, step, env, metrics):
-        base_name = env
         for key, value in metrics.items():
-            name = base_name + "." + key
-            wandb.log({name, value}, step=step)
+            name = env + "." + key
+            wandb.log({name: value}, step=step)
 
     def log_learning_rates(self, lrates, step):
         for index, rate in enumerate(lrates):
@@ -50,7 +52,7 @@ class WANDBLogger(BaseLogger):
     def log_epoch(self, step, env, loss, custom_metrics, lrates):
         if not self.log_index_batches:
             name = env + "." + "loss"
-            wandb.log({name: loss})
+            wandb.log({name: loss}, step=step)
             if lrates is not None:
                 self.log_learning_rates(lrates, step)
             self._log_info(step, env, loss, custom_metrics)
@@ -62,7 +64,7 @@ class WANDBLogger(BaseLogger):
 
     def add_artifact(self, filename):
         try:
-            wandb.save(filename)
+            wandb.save(os.path.join(filename))
             print("[LOGGER] \t Saved artifact to WANDB database {}"
                   .format(str(filename)))
         except Exception as e:
